6ad73869ecd79e140fb3a69f9da78503b2b59b1d
pckpct <pckpct@hotmail.com>
2013-05-14 10:51:38 +0200

    New CI

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,13 +38,9 @@
 |
 */
 
-/*
 $route['default_controller'] = "welcome";
 $route['404_override'] = '';
-*/
 
-$route['default_controller'] = "main";
-$route['404_override'] = '';
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
